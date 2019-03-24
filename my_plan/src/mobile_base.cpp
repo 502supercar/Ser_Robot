4c1b10e278cba2a4427a413db1341196e7bca512
wwf502 <32427761+wu568760312@users.noreply.github.com>
2019-03-24 18:55:02 +0800

    Add rotation function to the MobileBase Class.
    Add rotation function to the MobileBase Class. But it rotates slowly because of the cost of tf transform.

--- my_plan/src/mobile_base.cpp
+++ my_plan/src/mobile_base.cpp
@@ -8,6 +8,104 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+void Mobile_Base::getCurrentPose()
+{
+    //定义tf监听器
+    tf::TransformListener listener;
+
+    //定义存贮转换矩阵的变量
+    tf::StampedTransform transform;
+
+    try
+    {
+        //查找base与map的坐标转换
+        listener.waitForTransform("/map", "/base_link", ros::Time(0), ros::Duration(3.0));
+        listener.lookupTransform("/map", "/base_link", ros::Time(0), transform);
+    }
+    catch (tf::TransformException &ex)
+    {
+        ROS_ERROR("%s", ex.what());     //查找不到报错
+        ros::Duration(1.0).sleep();
+    }
+    //把监听到的移动平台位置信息存入类的私有数据
+    current_pose_.x = transform.getOrigin().x();
+    current_pose_.y = transform.getOrigin().y();
+
+    tf::Quaternion qua = transform.getRotation();   //获取四元数
+    double roll,pitch,yaw;
+    tf::Matrix3x3(qua).getRPY(roll, pitch, yaw);    //把四元数转换为角度值
+    current_pose_.theta = yaw;
+
+    //ROS_INFO("Current: x= %f, y= %f,theta= %f",current_pose_.x,current_pose_.y,current_pose_.theta);
+}
+
+bool Mobile_Base::carRotation(int angle,ros::NodeHandle &node_handle)
+{
+    //创建一个发布者，消息类型为geometry::Twist
+	ros::Publisher rotation_pub =node_handle.advertise<geometry_msgs::Twist>("/ir100_velocity_controller/cmd_vel",1000);
+
+    //设置循环的频率
+	ros::Rate loop_rate(10);
+
+    //初始化发送的消息变量
+    geometry_msgs::Twist tw;
+    //设置原地旋转的速度
+    tw.linear.x = 0.0;
+    tw.linear.y = 0.0;
+    if(angle > 0) tw.angular.z = ROT_ANG_V;
+	else if(angle < 0) tw.angular.z = -ROT_ANG_V;
+    else return 0;
+
+    getCurrentPose();
+    int abs_angle = abs(angle);
+    double start_offset = 0 - current_pose_.theta;
+    int round = abs_angle/180;
+    int remain_angle = abs_angle%180;
+    double angle_rad = remain_angle/180.0 * PI;
+
+    //执行完整圈数的旋转
+    double temp;
+    int n = 0;
+    double distance = 0;
+    while(round != 0)
+    {
+        getCurrentPose();
+        if(((current_pose_.theta >0) && (angle >0)) || ((current_pose_.theta) >0 && (angle <0))) 
+            temp = current_pose_.theta - PI;
+        else temp = current_pose_.theta + PI;
+        while(1)
+        {
+            getCurrentPose();
+            distance = fabs(current_pose_.theta - temp);
+            if(distance < 0.2) break;
+            //ROS_INFO("current_pose_.theta= %f,temp= %f,distance= %f",current_pose_.theta,temp,distance);
+            rotation_pub.publish(tw);
+        }
+       --round;
+    }
+
+    //执行剩下角度的旋转
+    double cur_angle = 0;
+    distance=0;
+    getCurrentPose();
+    start_offset = 0 - current_pose_.theta;
+    while(remain_angle != 0)
+    {
+        getCurrentPose();
+        cur_angle = current_pose_.theta + start_offset;
+        distance = fabs(angle_rad-fabs(cur_angle));
+        //ROS_INFO("cur_angle= %f,angle_rad= %f,distance= %f",cur_angle,angle_rad,distance);
+        if(distance < 0.2) break;
+        rotation_pub.publish(tw);
+    }
+
+    tw.angular.z = 0.0;
+    rotation_pub.publish(tw);
+    ros::spinOnce();
+    loop_rate.sleep();
+    return 1;
+}
+
 bool Mobile_Base::initRotation(ros::NodeHandle &node_handle)  //mobile_base initial rotation
 {
     if(ros::ok())
