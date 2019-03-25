39e76f370e7b93613e9d528c20835f0f9a07fe98
xiaomu502 <240972645@qq.com>
2019-03-25 15:53:04 +0800

    Update main.cpp

--- my_plan/src/main.cpp
+++ my_plan/src/main.cpp
@@ -1,49 +1,7 @@
 #include <my_pick_place.h>
 #include <mobile_base.h>
-/*
-double grasp_x=0.7;
-double grasp_y=0.5;
-double grasp_z=0.3;
 
-int main(int argc,char** argv)
-{ 
-    ros::init(argc, argv, "main");
-    ros::NodeHandle node;
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
-/*
-    Mobile_Base mobilebase;     //创建一个Mobile_Base对象
-
-    mobilebase.initRotation(node);        //初始化旋转定位
-
-    mobilebase.moveToGoal(0.5, 0.0, 0.0, node); */     //发送目标点
-
-    /*kinova::PickPlace pick_place(node);
-
-    if(pick_place.workspace(grasp_x,grasp_y,grasp_z))
-    {
-      pick_place.pick(grasp_x,grasp_y,grasp_z);
-    }
-    else
-    { 
-      return -1;
-    }
-
-
-    if(pick_place.workspace(0.4,-0.5,0.45))
-    {
-      pick_place.place(0.4,-0.5,0.45);
-    }
-    else
-    { 
-      return -1;
-    }
-
-    ros::spin();
-    return 0;
-}*/
-
-bool replan_flag;
+extern int mode;
 
 int main(int argc,char** argv)
 {
@@ -52,46 +10,72 @@ int main(int argc,char** argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  char a;
+  Mobile_Base mobilebase(node);     //创建一个Mobile_Base对象
+  kinova::PickPlace pick_place(node);
+
+  std::string a;
   std::cout<<"Please enter any key to start!"<<std::endl;
   std::cin>>a;
 
-  Mobile_Base mobilebase;     //创建一个Mobile_Base对象
-  kinova::PickPlace pick_place(node);
+  //replan_flag = false;
 
-  replan_flag = false;
+  mode=0;
 
   while(ros::ok())
   {
-    if(replan_flag == false)
+    //mode=0为小车模式，mode=1为机械臂pick模式,mode=2为机械臂place模式，mode=3为旋转调整位置模式
+    switch(mode)
     {
+      case 0:
         mobilebase.getGoalFromObject(1.6,0,0);
-        mobilebase.moveToGoal(node);
-    }
-    else
-    {
-      replan_flag = false;
-    }
-    if(pick_place.workspace(0.7,0.5,0.3))
-    {
-      pick_place.pick(0.7,0.5,0.3);
-    }
-    else 
-    {
-      replan_flag=true;
-      return -1;
-    }
-    if(pick_place.workspace(0.4,-0.5,0.45))
-    {
-      pick_place.place(0.4,-0.5,0.45);
-    }
-    else
-    { 
-      replan_flag=true;
-      return -1;
+        if(mobilebase.moveToGoal())
+        {
+          mode=1;
+        }
+        else
+        {
+          std::cout<<"Failed moving goal."<<std::endl;
+          return -1;
+        }
+        break;
+      case 1:
+        //workspace为false,则函数内部mode=2;
+        if(pick_place.workspace(0.7,0.5,0.3))
+        { 
+          //pick和place函数当规划失败时，可选择将mode置为2;
+          if(pick_place.pick(0.7,0.5,0.3)) mode=2;  
+          //mode=3;
+        }
+        break;
+      case 2:
+        if(pick_place.workspace(0.4,-0.5,0.45))
+        { 
+          //pick和place函数当规划失败时，可选择将mode置为2;
+          if(pick_place.place(0.4,-0.5,0.45)) mode=4;
+          //mode=3;
+        }
+        break;
+      case 3:
+        if(mobilebase.carRotation(30))
+        {
+          mode=1;
+        }
+        break;
+      default:
+        std::cout<<"Press w to start mode 0,other to stop the program.";
+        std::cin>>a;
+        if(a=="w" || a=="W")
+        {
+          mode=0;
+        }
+        else
+        {
+          return 1;
+        }
+        break;
     }
   }
+
   ros::spin();
   return 0;
 }
-
