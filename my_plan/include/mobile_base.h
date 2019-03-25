06f87160ed37e76965155cecd7afaaffad1475b6
wwf502 <32427761+wu568760312@users.noreply.github.com>
2019-03-25 11:10:11 +0800

    Edit the Mobile_Base Class

--- my_plan/include/mobile_base.h
+++ my_plan/include/mobile_base.h
@@ -5,9 +5,10 @@
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
 
-#define ROT_ANG_V 0.5      //the velocity of initial rotation
+#define ROT_ANG_V 0.8      //the velocity of initial rotation
 #define PI 3.14159
 
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 struct mobile_goal
 {
@@ -19,16 +20,23 @@ struct mobile_goal
 class Mobile_Base
 {
 private:
+    ros::NodeHandle nh_;
     mobile_goal target_pose_;
     mobile_goal current_pose_;
+
+    MoveBaseClient ac_;
+    ros::Publisher car_tw_pub_;
 public:
+    Mobile_Base(ros::NodeHandle &nh);
+    ~Mobile_Base();
+
     void getCurrentPose();
-    bool carRotation(int angle,ros::NodeHandle &node_handle);
-    bool initRotation(ros::NodeHandle &node_handle);
+    bool carRotation(int angle);
+    bool initRotation();
     void activeCb();
     void feedbackCb(const move_base_msgs::MoveBaseFeedbackConstPtr& move_base_feedback);
-    bool moveToGoal(ros::NodeHandle &node_handle);
     void getGoalFromObject(double x, double y, double z);
+    bool moveToGoal();
 };
 
 #endif
